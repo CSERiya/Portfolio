a6cebedb7f4e5d82581dcc1f6debae6b3664de77
CSERiya <93813209+CSERiya@users.noreply.github.com>
2023-11-30 21:16:31 +0530

    update

--- src/Components/Intro/intro.jsx
+++ src/Components/Intro/intro.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './intro.css';
-import My_img from '../../assets/My_pic.png'
+import My_img from '../../assets/My_pic.png'  
 import {Link} from 'react-scroll'
 import hire_img from '../../assets/hireme.png'
 
@@ -12,10 +12,11 @@ const Intro = () => {
 <span className="introText">I'm <span className="introName">Riya</span><br/>Website Developer</span>
 <p className='introPara'>I am skilled and passionate MERN stack developer with experience <br/> in building visually appealing and user-friendly
      websites.</p>
-    <Link><button className='btn'><img src={hire_img} alt='Hire me'/>Hire me</button></Link>
+    <Link><button className='btn'><img src={hire_img} alt='Hire me' className='btn_img'/>Hire me</button></Link>
         </div>
-        <img src={My_img} alt='Profile' className='bg'/>
-    </section>
+        <img src={My_img} alt='Profile' className='My_img'/>
+     </section>
+     
   );
 }
 
