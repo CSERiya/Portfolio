e2a5862b7d29d35676816cd58afa0ff5e5ad662d
CSERiya <93813209+CSERiya@users.noreply.github.com>
2023-12-19 23:34:22 +0530

    minor changes in CSS

--- src/Components/Intro/intro.jsx
+++ src/Components/Intro/intro.jsx
@@ -12,7 +12,7 @@ const intro = () => {
 <span className="introText">I'm <span className="introName">Riya</span><br/>Website Developer&#x1F469;&#x200D;&#x1F4BB;</span>
 <p className='introPara'>I am skilled and passionate MERN stack developer with experience <br/> in building visually appealing and user-friendly
      websites.</p>
-    <Link to=''><button className='btn'><img src={hire_img} alt='Hire me' className='btn_img'/>Hire me</button></Link> 
+    <Link to='contact' activeClass='active' spy={true} smooth={true} offset={-50} duration={500}><button className='btn'><img src={hire_img} alt='Hire me' className='btn_img'/>Hire me</button></Link> 
         </div>
         <img src={My_img} alt='Profile' className='My_img'/>
      </div>
