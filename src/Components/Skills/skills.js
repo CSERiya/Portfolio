c4cfee692f4e7dcf07bef6a678ad0cf8e8948305
CSERiya <93813209+CSERiya@users.noreply.github.com>
2024-09-03 12:01:44 +0530

    projects and resume updated

--- src/Components/Skills/skills.js
+++ src/Components/Skills/skills.js
@@ -8,29 +8,30 @@ const Skills = () => {
   return (
   <section id='skills'>
 <span className='skillTitle'> Skills</span>
-<span className="skillDesc">I am a skilled and passionate website developer with experience in HTML, CSS, JS, React.js. Working on 
-Backend part (MongoDB, Node.js, express.Js). Want to work more on Mern full stack development. And I simply enjoy coding and problem-solving.</span>
+<span className="skillDesc">I am a skilled and passionate website developer with experience in Frontend(HTML, CSS, JS, React.js) 
+    and Backend(MongoDB, Node.js, express.Js). Open to work as Mern full stack developer or Software Developer. And I simply enjoy 
+    coding and problem-solving.</span>
 <div className="skillBars">
 <div className="skillBar">
     <img src={webdev_img} alt='webdev' className='skillBarImg'/>
     <div className="skillBarText">
         <h2>Technical Skills</h2>
-<p>I am a fronted developer and working on backend development, so that I can work for MERN full stack domain.</p>
+<p>I am a full-stack developer and working on my personal and college projects.</p>
     </div>
 </div>
 <div className="skillBar">
     <img src={coding_img} alt='coding' className='skillBarImg'/>
     <div className="skillBarText">
         <h2>Coding Skills</h2>
-<p>Solved 2000+ DSA problems (languages C++ and Java) on platforms like Leetcode, Gfg, CodeStudio, etc. Consistency of 300+ days. Leetcode 
-    Global Rank- 32k, with various badges for problem solving and swags as appreciation.</p> 
+<p>Solved 2000+ DSA problems (languages C++ and Java) on platforms like Leetcode, Gfg, CodeStudio, etc. Consistency of 365+ days. Leetcode 
+    Global Rank- 9k, received various badges and swags for problem solving as appreciation.</p> 
     </div>
 </div>
 <div className="skillBar">
     <img src={work_img} alt='work' className='skillBarImg'/>
     <div className="skillBarText">
         <h2>Other Skills </h2>
-<p>OOP, OS, DBMS, Communication, Networking, LeaderShip, Teamwork, Time-management.</p>
+<p>OOP, OS, DBMS, AWS, Communication, Analytical, Networking, LeaderShip, Teamwork, Time-management.</p>
     </div>
 </div>
 </div>
--- src/Components/Works/works.js
+++ src/Components/Works/works.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './works.css'
-import img1 from '../../assets/Textutils.png'
+import img1 from '../../assets/Blogera.png'
 import img2 from '../../assets/Newshub.png'
 import img3 from '../../assets/Emart.png'
 
@@ -9,14 +9,19 @@ const works = () => {
     <div className='myworks'>
         <h2 className='workstitle'>My Projects</h2>
 <span className='worksDesc'>I have been actively engaged in various web development projects, focusing primarily on MERN full-stack applications
-. Having dedicated more than six months to honing my skills, I am now eager to 
+. Having dedicated more than a year to honing my skills, I am now eager to 
 transition to industry projects. I am seeking opportunities to expand my knowledge and gain valuable experience within a professional working 
 environment.</span>
 <div className='worksImgs'>
-        <img src={img1} className='workImgs' alt=''/>
-      <img src={img2} className='workImgs' alt=''/>
-      <img src={img3} className='workImgs' alt=''/>
-    
+<a href="https://github.com/CSERiya/Blogera" target="_blank" rel="noopener noreferrer">
+  <img src={img1} className="workImgs" alt="" />
+</a>
+<a href="https://github.com/CSERiya/Newsapp" target="_blank" rel="noopener noreferrer">
+  <img src={img2} className="workImgs" alt="" />
+</a>
+<a href="https://github.com/CSERiya/Emart-MERN" target="_blank" rel="noopener noreferrer">
+  <img src={img3} className="workImgs" alt="" />
+</a>
       </div>
       <div className='btn1'>
       <a href='https://github.com/CSERiya?tab=repositories' target='_blank'><button className='workBtn'>See More</button></a>
